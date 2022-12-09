0b04f24358d57e4079ca99c2a7b988b4e27d0a83
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-12-09 17:38:01 -0500

    Improved FileDownload events #8

--- src/lib/FileDownload.ts
+++ src/lib/FileDownload.ts
@@ -3,14 +3,22 @@ import { Readable }           from "stream"
 import EventEmitter           from "events"
 import request                from "./request"
 import { createDecompressor } from "./utils"
+import {
+    Options,
+    OptionsOfUnknownResponseBody,
+    Response
+} from "got/dist/source"
 
 
 const debug = util.debuglog("app-request")
 
 export interface FileDownloadState {
+    requestOptions   : Options
+    response        ?: Response
     downloadedChunks : number
     downloadedBytes  : number
     uncompressedBytes: number
+    error           ?: Error | string
 }
 
 export interface FileDownloadOptions {
@@ -19,9 +27,16 @@ export interface FileDownloadOptions {
     requestOptions?: OptionsOfUnknownResponseBody
 }
 
+export interface FileDownloadEvents {
+    start   : (this: FileDownload, state: FileDownloadState) => void
+    progress: (this: FileDownload, state: FileDownloadState) => void
+    error   : (this: FileDownload, error: FileDownloadState) => void
+    complete: (this: FileDownload, state: FileDownloadState) => void
+}
+
 interface FileDownload {
-    on(event: "progress", listener: (state: FileDownloadState) => void): this;
-    emit(event: "progress", state: FileDownloadState): boolean;
+    on<U extends keyof FileDownloadEvents>(event: U, listener: FileDownloadEvents[U]): this;
+    emit<U extends keyof FileDownloadEvents>(event: U, ...args: Parameters<FileDownloadEvents[U]>): boolean;
 }
 
 class FileDownload extends EventEmitter
@@ -38,11 +53,16 @@ class FileDownload extends EventEmitter
             downloadedChunks : 0,
             downloadedBytes  : 0,
             uncompressedBytes: 0,
+            requestOptions   : {}
         }
     }
 
     public getState() {
         return { ...this.state }
+    }
+
+    public emit(eventName: string | symbol, ...args: any[]): boolean {
+        return super.emit(eventName, this.getState(), ...args)
     }
 
     /**
@@ -60,6 +80,7 @@ class FileDownload extends EventEmitter
                 ...requestOptions,
                 decompress: false,
                 responseType: "buffer",
+                throwHttpErrors: false,
                 headers: {
                     ...requestOptions.headers,
                     "accept-encoding": "gzip, deflate, br, identity"
@@ -69,6 +90,10 @@ class FileDownload extends EventEmitter
             if (accessToken) {
                 options.headers.authorization = `Bearer ${accessToken}`
             }
+
+            this.state.requestOptions = { ...options, url: this.url }
+
+            this.emit("start")
 
             const downloadRequest = request.stream(this.url, options)
 
@@ -85,8 +110,15 @@ class FileDownload extends EventEmitter
                 });
             }
 
+            downloadRequest.once("end", () => this.emit("complete"));
+
             // In case the request itself fails --------------------------------
-            downloadRequest.on("error", reject)
+            // downloadRequest.on("error", reject)
+            downloadRequest.on("error", error => {
+                this.state.error = error
+                this.emit("error")
+                reject(error)
+            })
 
             // Count downloaded bytes ------------------------------------------
             downloadRequest.on("data", (data: string) => {
@@ -98,9 +130,7 @@ class FileDownload extends EventEmitter
                 
                 // In case we get an error response ----------------------------
                 if (res.statusCode >= 400) {
-                    return reject(new Error(
-                        `${this.url} -> ${res.statusCode}: ${res.statusMessage}\n${res.body || ""}`
-                    ));
+                    this.emit("error");
                 }
 
                 // Un-compress the response if needed --------------------------
@@ -110,7 +140,7 @@ class FileDownload extends EventEmitter
                 // Count uncompressed bytes ------------------------------------
                 decompress.on("data", (data: string) => {
                     this.state.uncompressedBytes += data.length
-                    this.emit("progress", this.state)
+                    this.emit("progress")
                 });
                 
                 // Count incoming raw data chunks ------------------------------
