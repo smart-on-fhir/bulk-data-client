636525227042f5e0d3429117cf09c75cca5b6f9d
Dylan Phelan <Dtphelan1@gmail.com>
2023-12-29 12:55:34 -0500

    revert config file changes

--- config/defaults.js
+++ config/defaults.js
@@ -14,32 +14,18 @@
      * FHIR server base URL. Can be overridden by the `-f` or `--fhir-url`
      * CLI parameter.
      */
-    fhirUrl: "http://localhost:9443/eyJlcnIiOiIiLCJwYWdlIjoxMDAwMCwiZHVyIjoxMCwidGx0IjoxNSwibSI6MSwic3R1IjozLCJkZWwiOjB9/fhir",
+    fhirUrl: "",
 
     /**
      * The Bulk Data server token URL ("none" for open servers)
      */
-    tokenUrl: "http://localhost:9443/auth/token",
+    tokenUrl: "",
  
     /**
      * The private key (JWK) used to sign authentication tokens. This is not
      * needed for open servers
      */
-    privateKey: {
-         kty: "RSA",
-         alg: "RS384",
-         n: "ucG7qkdpxlJ359PKMpYS7Lmr5acNLswpHJqouuteBuym_74UkwmCmD-QjvDIZ2iYI5lcou74NcXnv00oEisp9pscoOLyNPVM4qF6Bby4t7xr3EgXpWEeOF0U6F_UNgOahumTWgv9ypYG6rYEucl5fYZaY81KznTMxe3lXWQAv3cdSRBoNb0DpFULWGqR2GwiOgtGR-EQWL0OeNBBQEAQuM6-5uxN2i0hfTLdUOjeXGdW1VGJ7mzMvPWkm7W9js8gJjgwOKQirfsxPAw2gV8JB_qCvg5dx3lMjqxe-qUPi54sGOwm9924gOxePS2HI1HaPtxr_zQSy5HfYue1arDguw",
-         e: "AQAB",
-         d: "FIAIS_8xnIzWus5UJEe3svRoFMzeLD9P0jOnUhROU4lOgi7iFRAZn4p0U7IdtnvZPnnbEAg7ewO_qGHFfavR0mtv5LmRwr0c98NGX4JiXbSnsASCHOLzni4iSTrk2p6EhpsZ2KLg0WNumVHugj_KmUsjMpx3ba1_josgowaO--eN5KIXOo8NWgaDvNF7doXf7nbHQpCCAbEOkEkuihEwTdnQWkfkTLmfo1Qrdj5MeW4ndp8x8IhwAwgX0TylsTXSOzv0SyDvx_hCsj7ae9iERlCkhcchPVlYix2d7c3paAQRQYdYLrmcFjzjC2_z7HHJXkX5F0uNHJJ127ni0YApQQ",
-         p: "4aiaVXZsGWhx90NBsmiIxJaq9D6lvs03wOP3l77NR3upqHqUEWwCOyxHArG_mAMr1IQOemwHOvgL2o39-YU1wWg0keU_3pf0ts-n1ZoMiNZvg-mdr_7PrGUa4AfmNMY1jJQe0qB9KVj938OCp_3Pn_ouwGRpypZmzFzkv3oyaTE",
-         q: "0rurMbTscZHatcRSUnh0HXeIO24dlxPG_LEZvG7jL_cnLjSNtYezrTO9MvPz05uQYM5rOc3ee6F3pVZa_4GO3aGY4kr-soiP25-o8rrOo3DqJ5cHx4LFr5QH3evuhi4B9M-zPeYArk8XV3Vif_GPJEAfvEQk5wI8_hYvL2U3Las",
-         dp: "GzVXFoLm2dFsHGIxo69S_lEkX7rGBVF9LXbPPa611a6lcDkHbWpWgof-L-b6sPuA52jczgoEfSm3VCzWuKVFLALCg-zeXJp52SkElY6zgDRK0d7zMmtI4wJ10Rlium5DuqWJaeAL91ZRlg9ey56g0Cs8Q9pXnyOvVWjF9Ahp16E",
-         dq: "dQ2fSw33JCJjPQHexEZ6IQALYQ-KSifHKQdqhzuE4FjFn6m7aTEGgdeyaUIOluTbLpqZ_tK2mS-YWPN9ul0JsVwYouILVbn7RoAKBUH0k96dgf4naQ_fpOZx9DggFtIpbgWMx34htLDkA0WFwBG6c-VWe2nSoaqhnHVQImBzZN8",
-         qi: "2j3PPP_tbF8gYpeFxxf8BhEfUJ8kCB8ufiSYtEb2PeuWK-wGCAVohgN_vl8UDwSi895fLBA0QFO6nK0UhWWG6FzwXB2V1t_nVlGmjB4wdV52NfT9UkV5Y6gbrE25weBM1zz9XywSk2Uye4JLaz2uJu1fF-odkkWCDRZSUuwvI_c",
-         key_ops: ["sign"],
-         ext: true,
-         kid: "9508fac4ca6791dd8d8370377147d767",
-    }, 
+    privateKey: {},
 
     /**
      * This is not needed for open servers
@@ -324,6 +310,6 @@
     /**
      * How long to wait in between file download attempts, in milliseconds
      * Applies to FileDownloads and to bulk-client's attachment downloads
-     */
+    */
     fileDownloadRetryAfterMSec: 1000,
 }
